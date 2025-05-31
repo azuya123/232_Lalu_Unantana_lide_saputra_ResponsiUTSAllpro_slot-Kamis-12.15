ef0e2ecdeb27507af6549279a7dc250635eb8cf0
azuya123 <2400018232@webmail.uad.ac.id>
2025-05-31 14:34:54 +0700

    Add files via upload

---
+++ 232 RESPONSI UTS ALPRO 12.15 .B.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string nim,nama,prodi,fakultas;
+	
+	cout<<"Informasi peibadi"<<endl;
+	cout<<"NIm       :";
+	cin>>nim;
+	cout<<"Nama      :";
+	cin>>nama;
+	cout<<"Prodi     :";
+	cin>>prodi;
+	cout<<"Fakultas  :";
+	cin>>prodi;
+}
+
+
+
+
+
+
+
